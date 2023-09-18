04e9d6925b32b21f1fca9fa77b5462580725e657
leviosacz <c.expecto.patronum@gmail.com>
2023-09-18 00:05:55 -0700

    Add transaction manager

---
+++ src/main/java/com/github/leviosacz/javadb/backend/tm/TransactionManagerImpl.java
@@ -0,0 +1,173 @@
+package com.github.leviosacz.javadb.backend.tm;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TransactionManagerImpl implements TransactionManager {
+
+    // XID file header length
+    static final int XID_HEADER_SIZE = 8;
+    // The length of each transaction
+    private static final int XID_FIELD_SIZE = 1;
+
+    // Three states of a transaction
+    private static final byte FIELD_TRAN_ACTIVE   = 0;
+    private static final byte FIELD_TRAN_COMMITTED = 1;
+    private static final byte FIELD_TRAN_ABORTED  = 2;
+
+    // Super transaction, always in committed state
+    public static final long SUPER_XID = 0;
+
+    static final String XID_SUFFIX = ".xid";
+
+    private RandomAccessFile file;
+    private FileChannel fc;
+    private long xidCounter;
+    private Lock counterLock;
+
+    TransactionManagerImpl(RandomAccessFile raf, FileChannel fc) {
+        this.file = raf;
+        this.fc = fc;
+        counterLock = new ReentrantLock();
+        checkXIDCounter();
+    }
+
+    // Start a transaction and return XID
+    public long begin() {
+        counterLock.lock();
+        try {
+            long xid = xidCounter + 1;
+            updateXID(xid, FIELD_TRAN_ACTIVE);
+            incrXIDCounter();
+            return xid;
+        } finally {
+            counterLock.unlock();
+        }
+    }
+    // Commit a transaction
+    public void commit(long xid) {
+        updateXID(xid, FIELD_TRAN_COMMITTED);
+    }
+
+    // Abort a transaction
+    public void abort(long xid) {
+        updateXID(xid, FIELD_TRAN_ABORTED);
+    }
+
+    // Check whether the transaction is active
+    public boolean isActive(long xid) {
+        if(xid == SUPER_XID) return false;
+        return checkXID(xid, FIELD_TRAN_ACTIVE);
+    }
+
+    // Check whether the transaction is committed
+    public boolean isCommitted(long xid) {
+        if(xid == SUPER_XID) return true;
+        return checkXID(xid, FIELD_TRAN_COMMITTED);
+    }
+
+    // Check whether the transaction is aborted
+    public boolean isAborted(long xid) {
+        if(xid == SUPER_XID) return false;
+        return checkXID(xid, FIELD_TRAN_ABORTED);
+    }
+
+    // Close the XID file
+    public void close() {
+        try {
+            fc.close();
+            file.close();
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+    }
+
+    /**
+     * Check whether the XID file is legal.
+     * Read the xidcounter in XID_FILE_HEADER, calculate the theoretical length of the file,
+     * and compare it with the actual length of the file.
+     */
+    private void checkXIDCounter() {
+        long fileLen = 0;
+        try {
+            fileLen = file.length();
+        } catch (IOException e1) {
+            Panic.panic(Error.BadXIDFileException);
+        }
+        if(fileLen < XID_HEADER_LENGTH) {
+            Panic.panic(Error.BadXIDFileException);
+        }
+
+        ByteBuffer buf = ByteBuffer.allocate(XID_HEADER_LENGTH);
+        try {
+            fc.position(0);
+            fc.read(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        this.xidCounter = Parser.parseLong(buf.array());
+        long end = getXidPosition(this.xidCounter + 1);
+        if(end != fileLen) {
+            Panic.panic(Error.BadXIDFileException);
+        }
+    }
+
+    // Return the transaction's location in the xid file based on its xid
+    private long getXidPosition(long xid) {
+        return LEN_XID_HEADER_LENGTH + (xid-1)*XID_FIELD_SIZE;
+    }
+
+    // Update the status of transaction with xid to status
+    private void updateXID(long xid, byte status) {
+        long offset = getXidPosition(xid);
+        byte[] tmp = new byte[XID_FIELD_SIZE];
+        tmp[0] = status;
+        ByteBuffer buf = ByteBuffer.wrap(tmp);
+        try {
+            fc.position(offset);
+            fc.write(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        try {
+            fc.force(false);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+    }
+
+    // Increase XID Counter and update XID Header
+    private void incrXIDCounter() {
+        xidCounter ++;
+        ByteBuffer buf = ByteBuffer.wrap(Parser.long2Byte(xidCounter));
+        try {
+            fc.position(0);
+            fc.write(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        try {
+            fc.force(false);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+    }
+
+    // Check whether the transaction with xid is in status state
+    private boolean checkXID(long xid, byte status) {
+        long offset = getXidPosition(xid);
+        ByteBuffer buf = ByteBuffer.wrap(new byte[XID_FIELD_SIZE]);
+        try {
+            fc.position(offset);
+            fc.read(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        return buf.array()[0] == status;
+    }
+
+}
