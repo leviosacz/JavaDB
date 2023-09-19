2aa2d686e29a5d0ed0fef4f6e2cd8262933b27eb
leviosacz <c.expecto.patronum@gmail.com>
2023-09-18 19:18:47 -0700

    Add abstract cache and subarray

--- src/main/java/com/github/leviosacz/javadb/backend/tm/TransactionManager.java
+++ src/main/java/com/github/leviosacz/javadb/backend/tm/TransactionManager.java
@@ -8,6 +8,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 public interface TransactionManager {
+
     long begin();
     void commit(long xid);
     void abort(long xid);
