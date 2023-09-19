86e37ebce05872958ecea19c169354bece3afaca
leviosacz <c.expecto.patronum@gmail.com>
2023-09-18 19:18:47 -0700

    Add abstract cache and subarray

---
+++ src/main/java/com/github/leviosacz/javadb/backend/common/AbstractCache.java
@@ -0,0 +1,130 @@
+package com.github.leviosacz.javadb.backend.common;
+
+import java.util.HashMap;
+import java.util.Set;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * AbstractCache implements a reference counting cache.
+ */
+public abstract class AbstractCache<T> {
+    private HashMap<Long, T> cache;                     // actually cached data
+    private HashMap<Long, Integer> references;          // the number of references to a resource
+    private HashMap<Long, Boolean> getting;             // the thread that is getting a resource
+
+    private int maxResource;                            // the maximum number of cached resources
+    private int count = 0;                              // the number of cached resources
+    private Lock lock;
+
+    public AbstractCache(int maxResource) {
+        this.maxResource = maxResource;
+        cache = new HashMap<>();
+        references = new HashMap<>();
+        getting = new HashMap<>();
+        lock = new ReentrantLock();
+    }
+
+    protected T get(long key) throws Exception {
+        while(true) {
+            lock.lock();
+            if(getting.containsKey(key)) {
+                // The requested resource is being got by another thread
+                lock.unlock();
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    continue;
+                }
+                continue;
+            }
+
+            if(cache.containsKey(key)) {
+                // The resource is in the cache and is returned directly
+                T obj = cache.get(key);
+                references.put(key, references.get(key) + 1);
+                lock.unlock();
+                return obj;
+            }
+
+            // Try to get the resource
+            if(maxResource > 0 && count == maxResource) {
+                lock.unlock();
+                throw Error.CacheFullException;
+            }
+            count ++;
+            getting.put(key, true);
+            lock.unlock();
+            break;
+        }
+
+        T obj = null;
+        try {
+            obj = getForCache(key);
+        } catch(Exception e) {
+            lock.lock();
+            count --;
+            getting.remove(key);
+            lock.unlock();
+            throw e;
+        }
+
+        lock.lock();
+        getting.remove(key);
+        cache.put(key, obj);
+        references.put(key, 1);
+        lock.unlock();
+
+        return obj;
+    }
+
+    /**
+     * Forcefully release a cached resource
+     */
+    protected void release(long key) {
+        lock.lock();
+        try {
+            int ref = references.get(key)-1;
+            if(ref == 0) {
+                T obj = cache.get(key);
+                releaseForCache(obj);
+                references.remove(key);
+                cache.remove(key);
+                count --;
+            } else {
+                references.put(key, ref);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * Turn off caching and write back all resources
+     */
+    protected void close() {
+        lock.lock();
+        try {
+            Set<Long> keys = cache.keySet();
+            for (long key : keys) {
+                T obj = cache.get(key);
+                releaseForCache(obj);
+                references.remove(key);
+                cache.remove(key);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * Fetch when the resource is not in the cache
+     */
+    protected abstract T getForCache(long key) throws Exception;
+    /**
+     * Writeback when resources are evicted from the cache
+     */
+    protected abstract void releaseForCache(T obj);
+
+}
